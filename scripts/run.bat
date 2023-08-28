a2a792c84f45f366496cc5a3193cfa0c62757761
Jessica <gsk@gmail.com>
2023-08-28 10:15:06 -0300

    [NADAC] resultado csv

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,8 +5,8 @@ set PYTHONPATH=C:\GitHub\rpa\nadac_proj\
 
 set UrlNadac=https://data.medicaid.gov/dataset/4a00010a-132b-4e4d-a611-543c9521280f
 set ChromeDriver=C:\GitHub\chrome_driver\chromedriver.exe
-set DirInput=C:\Testes\nadac\input
-set DirOutput=C:\Testes\nadac\output
+set DirInput=C:\Testes\nadac_proj\input
+set DirOutput=C:\Testes\nadac_proj\output
 set BaseFile=C:\BD\nadac_proj\nadac.db
 set Qtde=100
 
