7c0ed55d4140b858ef619dbf619f0a0ff0e3ff9d
Jessica <gsk@gmail.com>
2023-08-28 01:58:07 -0300

    [NADAC] ajuste caminhos de pastas

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,13 +1,13 @@
 @echo off
 
-cd C:\GitHub\rpa\nadac_project\
-set PYTHONPATH=C:\GitHub\rpa\nadac_project\
+cd C:\GitHub\rpa\nadac_proj\
+set PYTHONPATH=C:\GitHub\rpa\nadac_proj\
 
 set UrlNadac=https://data.medicaid.gov/dataset/4a00010a-132b-4e4d-a611-543c9521280f
 set ChromeDriver=C:\GitHub\chrome_driver\chromedriver.exe
 set DirInput=C:\Testes\nadac\input
 set DirOutput=C:\Testes\nadac\output
-set BaseFile=C:\BD\nadac_project\nadac.db
+set BaseFile=C:\BD\nadac_proj\nadac.db
 set Qtde=100
 
 taskkill /IM python.exe /f
